050148c747d365afa08282173562d1537e3d8ea4
Ragini340 <88105537+Ragini340@users.noreply.github.com>
2024-11-12 08:39:49 +0530

    Add files via upload

---
+++ ClassAndObject.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Practice
+{
+    public class ClassAndObject
+    {
+        // Instance Variables
+        String name;
+        String breed;
+        int age;
+        String color;
+
+        // Constructor Declaration of Class
+        public ClassAndObject(String name, String breed,
+                    int age, String color)
+        {
+            this.name = name;
+            this.breed = breed;
+            this.age = age;
+            this.color = color;
+        }
+
+        // Property 1
+        public String GetName()
+        {
+            return name;
+        }
+
+        // Property 2
+        public String GetBreed()
+        {
+            return breed;
+        }
+
+        // Property 3
+        public int GetAge()
+        {
+            return age;
+        }
+
+        // Property 4
+        public String GetColor()
+        {
+            return color;
+        }
+
+        // Method 1
+        public String ToString()
+        {
+            return ("My name is " + this.GetName()
+                    + ".\nMy breed, age and color are " + this.GetBreed()
+                    + ", " + this.GetAge() + ", " + this.GetColor());
+        }
+
+        // Main Method
+       /* public static void Main(String[] args)
+        {
+            // Creating object
+            ClassAndObject tuffy = new ClassAndObject("Rag", "Abc", 5, "white");
+            Console.WriteLine(tuffy.ToString());
+        }*/
+    }
+
+}
