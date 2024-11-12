d83dd0420a34898a39ee41fd347a03886275e43d
Ragini340 <88105537+Ragini340@users.noreply.github.com>
2024-11-12 08:40:34 +0530

    Add files via upload

---
+++ ConstructorPOC.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Practice
+{
+    public class ConstructorPOC
+    {
+        int num;
+        string name;
+        int age;
+
+        //Default constructor
+        ConstructorPOC()
+        {
+            Console.WriteLine("Default constructor");
+        }
+
+        //Parametrized constructor with 2 parameters
+        ConstructorPOC(int num, string name)
+        {
+            this.num = num;
+            this.name = name;
+        }
+
+        //Parametrized constructor with 3 parameters
+        ConstructorPOC(int num, string name, int age)
+        {
+            this.num = num;
+            this.name = name;
+            this.age = age;
+        }
+
+        public static void Main()
+        {
+            ConstructorPOC constructorPOC = new ConstructorPOC();
+            Console.WriteLine(constructorPOC.num);
+            Console.WriteLine(constructorPOC.name);
+            Console.WriteLine(constructorPOC.age);
+        }
+
+    }
+}
