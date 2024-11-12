31b54e4ff41e1071414a0ecd094700aeacd17352
Ragini340 <88105537+Ragini340@users.noreply.github.com>
2024-11-12 20:43:57 +0530

    Add files via upload

---
+++ PrivateConstructor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Practice
+{
+    public class PrivateConstructor
+    {
+        //Declaring private constructor
+        private PrivateConstructor()
+        {
+        }
+
+        //Declaring static variable field
+        public static int count;
+
+        //Declaring static method
+        public static int CountMethod()
+        {
+            return count++;
+        }
+
+        public static void Main()
+        {
+            PrivateConstructor.count = 99;
+
+            // Accessing without any instance of the class
+            PrivateConstructor.CountMethod();
+            Console.WriteLine(PrivateConstructor.count);
+
+            // Accessing without any instance of the class
+            PrivateConstructor.CountMethod();
+            Console.WriteLine(PrivateConstructor.count);
+        }
+
+    }
+}
